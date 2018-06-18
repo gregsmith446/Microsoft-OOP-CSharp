a16a9fb4aaa1b46319a70e5940d2cbbf12f70fd9
gregsmith446 <gregsmith446@gmail.com>
2018-06-18 08:46:53 +0000

    mod2 notes

---
+++ module1/selfAssessment/module2/anon.cs
@@ -0,0 +1,11 @@
+//anonymous class demo
+
+#region Anon Class
+
+// create an anonymous object
+var anon = new { Name = "greg", age = "26" };
+
+// print the public fields to the console
+Console.WriteLine(anon.Name + anon.Age);
+
+#endregion
